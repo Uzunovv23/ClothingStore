0c0faadc0b635f4b6aed92c32fd063af9d7dc89f
Nikolay Uzunov <nikiuzunovv1122@gmail.com>
2024-12-27 15:12:58 +0200

    Initial commit, spring initial initialization

---
+++ ClothingStore/src/test/java/com/nnmfashion/clothingstore/ClothingStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnmfashion.clothingstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
